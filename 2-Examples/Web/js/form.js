bbcdb183773e820e9ec54e0552b347715ac35f92
manhph-macos <manhph@manhph-macoss-MacBook-Pro.local>
2022-11-16 20:33:31 +0700

    update user

--- 2-Examples/Web/js/form.js
+++ 2-Examples/Web/js/form.js
@@ -1,18 +1,17 @@
 jQuery.validator.addMethod(
   "regex",
   function (value, element, regexp) {
-      if (regexp.constructor != RegExp)
-          regexp = new RegExp(regexp);
-      else if (regexp.global)
-          regexp.lastIndex = 0;
-      return this.optional(element) || regexp.test(value);
-  }, "erreur expression reguliere"
+    if (regexp.constructor != RegExp) regexp = new RegExp(regexp);
+    else if (regexp.global) regexp.lastIndex = 0;
+    return this.optional(element) || regexp.test(value);
+  },
+  "erreur expression reguliere"
 );
 let settings = {
   rules: {
     name: {
       required: true,
-      regex: /^[A-Za-z]+$/
+      regex: /^[a-zA-Z0-9_ ]*$/,
     },
     avatar: {
       required: true,
@@ -21,7 +20,7 @@ let settings = {
   messages: {
     name: {
       required: "Bạn chưa nhập email",
-      regex : "Khong duoc nhap chu so"
+      regex: "Khong duoc nhap chu so",
     },
     avatar: {
       required: "Yeu cau nhap password",
@@ -40,7 +39,14 @@ function handleSubmit() {
       name: inputName.value,
       avatar: inputAvatar.value,
     };
-    postUser(data);
+    let url = new URL(window.location.href);
+    let id = url.searchParams.get("id");
+    if (id) {
+      updateUser(id, data)
+    }else{
+      postUser(data);
+    }
+    
   } else {
     console.log("cancel submit");
   }
@@ -62,4 +68,54 @@ function postUser(data) {
     .catch((error) => {
       console.error("Error:", error);
     });
+}
+
+function updateUser(userId, data) {
+  let url = "https://63284e93a2e90dab7bdd0fd7.mockapi.io/api/v1/users/"+ userId;
+  fetch(url, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+window.onload = function (e) {
+  let url = new URL(window.location.href);
+  let id = url.searchParams.get("id");
+  if (id) {
+    getUser(id)
+  }
+  
+};
+
+function getUser(userId) {
+  let url =
+    "https://63284e93a2e90dab7bdd0fd7.mockapi.io/api/v1/users/" + userId;
+  fetch(url, {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      autoFillUser(data)
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+function autoFillUser(user) {
+  console.log(user);
+  let inputName = document.getElementById('inputName') // get elm
+  inputName.value = user.name
+  let inputAvatar = document.getElementById('inputAvatar')
+  inputAvatar.value = user.avatar
 }
