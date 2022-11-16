61de2c8f860669da3897ff4b1aba4ddacec1f838
manhph-macos <manhph@manhph-macoss-MacBook-Pro.local>
2022-11-16 19:04:15 +0700

    add form

--- 2-Examples/Web/js/form.js
+++ 2-Examples/Web/js/form.js
@@ -1,34 +1,53 @@
 let settings = {
   rules: {
-    email: {
+    name: {
       required: true,
-      email: true,
-      minlength: 6,
     },
-    password: {
+    avatar: {
       required: true,
-      minlength: 6,
-      maxlength: 12,
     },
-    address:{
-
-    }
   },
   messages: {
-    email: {
+    name: {
       required: "Bạn chưa nhập email",
-      minlength: jQuery.validator.format("Toi thieu co {0} ky tu"),
-      email: "Dia chi email khong hop le",
     },
-    password: {
+    avatar: {
       required: "Yeu cau nhap password",
-      minlength: "Toi thieu {0} ki tu",
-      maxlength: "Toi da {0} ki tu",
     },
-    address:{
-        
-    }
   },
 };
 
 let validator = $("#myForm").validate(settings);
+
+function handleSubmit() {
+  let isValid = $("#myForm").valid();
+  if (isValid) {
+    let inputName = document.getElementById("inputName");
+    let inputAvatar = document.getElementById("inputAvatar");
+    let data = {
+      name: inputName.value,
+      avatar: inputAvatar.value,
+    };
+    postUser(data);
+  } else {
+    console.log("cancel submit");
+  }
+}
+
+function postUser(data) {
+  let url = "https://63284e93a2e90dab7bdd0fd7.mockapi.io/api/v1/users";
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
